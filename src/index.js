68651a1b921f335afbba2567422f9f32fec5add5
theGordHoard <electricgord@gmail.com>
2017-11-28 20:09:59 -0700

    Export postition

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 const Velocity = require('./Velocity')
 const Acceleration = require('./Acceleration')
+const Position = require('./Position')
 const Object = require('./PhysicsObject')
 const System = require('./PhysticsSystem')
 
 module.exports = {
   Velocity: Velocity,
   Acceleration: Acceleration,
+  Position: Position,
   Object: Object,
   System: System
 }
