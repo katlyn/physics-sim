7cf8f3945bac0296e6ad9a7869feaa2854641585
theGordHoard <electricgord@gmail.com>
2017-11-28 20:26:11 -0700

    Add position calculations

--- src/PhysicsSystem.js
+++ src/PhysicsSystem.js
@@ -10,6 +10,12 @@ class PhysicsSystem {
   tick (time) {
     for (let key in this.objects) {
       let object = this.objects[key]
+
+      // Calculate positions
+      object.position.x = object.position.x + object.velocity.x * time + 0.5 * object.acceleration.x * Math.pow(time, 2)
+      object.position.y = object.position.y + object.velocity.y * time + 0.5 * object.acceleration.y * Math.pow(time, 2)
+
+      // Calulate velocities
       object.velocity.x = object.velocity.x + object.acceleration.x * time
       object.velocity.y = object.velocity.y + object.acceleration.y * time
     }
