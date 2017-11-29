1f4fe40c59b8b560f6e7e44362ef65d4e3bc0818
theGordHoard <electricgord@gmail.com>
2017-11-28 19:26:04 -0700

    Export everything

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const Velocity = require('./Velocity')
+const Acceleration = require('./Acceleration')
+const Object = require('./PhysicsObject')
+const System = require('./PhysticsSystem')
+
+module.exports = {
+  Velocity: Velocity,
+  Acceleration: Acceleration,
+  Object: Object,
+  System: System
+}
